9c34c861bfa9c5886cad15055ddc92e947246c33
Pon54 <psherjay@gmail.com>
2025-12-15 20:19:10 +0800

    commit

--- legacy/my-booking.php
+++ legacy/my-booking.php
@@ -195,14 +195,14 @@ foreach($results as $result)
 <!-- Scripts --> 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
 <script src="assets/js/interface.js"></script> 
 <!--Switcher-->
 <script src="assets/switcher/js/switcher.js"></script>
 <!--bootstrap-slider-JS--> 
 <script src="assets/js/bootstrap-slider.min.js"></script> 
 <!--Slider-JS--> 
-<script src="assets/js/slick.min.js"></script> 
-<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
 </body>
 </html>
 <?php } ?>
